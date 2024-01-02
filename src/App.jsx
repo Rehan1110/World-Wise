685528acf5f9a5893062d4a3b3934682328292e1
Rehan Abbas <abbasrehan054@gmail.com>
2024-01-02 15:49:07 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,7 @@ const Login = lazy(() => import("./pages/Login"));
 const AppLayout = lazy(() => import("./pages/AppLayout"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
-// dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
-// dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
+
 
 function App() {
   return (
